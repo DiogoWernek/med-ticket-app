761fa33769ace1123190bb8082778c90841d248c
DiogoWernek <diogowernek86@gmail.com>
2025-06-20 19:34:10 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-primary">
+      <h1 className="text-3xl font-rubik font-bold text-white underline decoration-secondary">
+        MEDTICKET
+      </h1>
+    </div>
+  )
+}
+
+export default App
